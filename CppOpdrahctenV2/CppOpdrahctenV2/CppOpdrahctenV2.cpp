e77c0ec0505f4be8d6835b38c73f93b8e36fe8d8
Oliver Norton <o.k.norton21@gmail.com>
2023-06-11 13:57:35 +0200

    Les 1 Opdracht 3

--- CppOpdrahctenV2/CppOpdrahctenV2/CppOpdrahctenV2.cpp
+++ CppOpdrahctenV2/CppOpdrahctenV2/CppOpdrahctenV2.cpp
@@ -1,80 +1,33 @@
-#include <iostream>
-#include <string>
-#include <iostream>
-#include <string>
-using namespace std;
-bool isValid(const std::string& cardNumber);
-int sumOfDoubleEvenPlace(const std::string& cardNumber);
-int getDigit(int number);
-int sumOfOddPlace(const std::string& cardNumber);
-bool startsWith(const std::string& cardNumber, const std::string& substr);
-std::string cardProvider;
+#include <stdio.h>
+#include <ctype.h>
+
+void count(const char s[], int counts[]) {
+    for (int i = 0; i < 26; i++) {
+        counts[i] = 0;
+    }
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        char c = tolower(s[i]);
+        if (isalpha(c)) {
+            counts[c - 'a']++;
+        }
+    }
+}
 
 int main() {
-    string cardNumber;
-    cout << "Enter a credit card number: ";
-    cin >> cardNumber;
-    if (isValid(cardNumber)) {
-        cout << cardNumber << " is a valid " << cardProvider << " card-number.\n";
-    }
-    else {
-        cout << cardNumber << "Is an invalid card number.\n";
+    char s[100];
+    int counts[26];
+
+    printf("Enter a string: ");
+    fgets(s, sizeof(s), stdin);
+
+    count(s, counts);
+
+    for (int i = 0; i < 26; i++) {
+        if (counts[i] > 0) {
+            printf("%c: %d times\n", 'a' + i, counts[i]);
+        }
     }
 
     return 0;
-}
-
-bool isValid(const string& cardNumber) {
-    if (cardNumber.length() < 13 || cardNumber.length() > 16) {
-        return false;
-    }
-
-    if (startsWith(cardNumber, "4")) { // Visa
-        return (sumOfDoubleEvenPlace(cardNumber) + sumOfOddPlace(cardNumber)) % 10 == 0;
-    }
-    else if (startsWith(cardNumber, "5")) { // MasterCard kaart
-        cardProvider = "Mastercard";
-        return (sumOfDoubleEvenPlace(cardNumber) + sumOfOddPlace(cardNumber)) % 10 == 0;
-    }
-    else if (startsWith(cardNumber, "37")) { // American Express kaart
-        cardProvider = "American Express";
-        return (sumOfDoubleEvenPlace(cardNumber) + sumOfOddPlace(cardNumber)) % 10 == 0;
-    }
-    else if (startsWith(cardNumber, "6")) { // Discover kaart
-        cardProvider = "Discover";
-        return (sumOfDoubleEvenPlace(cardNumber) + sumOfOddPlace(cardNumber)) % 10 == 0;
-    }
-    else {
-        return false;
-    }
-}
-
-int sumOfDoubleEvenPlace(const string& cardNumber) {
-    int sum = 0;
-    for (int i = cardNumber.length() - 2; i >= 0; i -= 2) {
-        int doubleVal = (cardNumber[i] - '0') * 2;
-        sum += getDigit(doubleVal);
-    }
-    return sum;
-}
-
-int getDigit(int number) {
-    if (number < 10) {
-        return number;
-    }
-    else {
-        return (number % 10) + (number / 10);
-    }
-}
-
-int sumOfOddPlace(const string& cardNumber) {
-    int sum = 0;
-    for (int i = cardNumber.length() - 1; i >= 0; i -= 2) {
-        sum += cardNumber[i] - '0';
-    }
-    return sum;
-}
-
-bool startsWith(const string& cardNumber, const string& substr) {
-    return cardNumber.find(substr) == 0;
 }
