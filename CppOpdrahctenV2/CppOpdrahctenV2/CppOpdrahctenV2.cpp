686d8a30b77d0d27c60058bcea2f75a9ef8cdce0
Oliver Norton <o.k.norton21@gmail.com>
2023-06-11 15:06:43 +0200

    Les 3 opdracht 4

--- CppOpdrahctenV2/CppOpdrahctenV2/CppOpdrahctenV2.cpp
+++ CppOpdrahctenV2/CppOpdrahctenV2/CppOpdrahctenV2.cpp
@@ -1,41 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iterator>
 
 template<typename T>
-std::vector<T> intersect(const std::vector<T>& v1, const std::vector<T>& v2) {
-    std::vector<T> result;
-    for (const auto& element : v1) {
-        if (std::find(v2.begin(), v2.end(), element) != v2.end()) {
-            result.push_back(element);
-        }
-    }
-    return result;
+void removeDuplicates(std::vector<T>& v) {
+    std::sort(v.begin(), v.end());
+    auto last = std::unique(v.begin(), v.end());
+    v.erase(last, v.end());
 }
 
 int main() {
-    std::vector<std::string> vector1;
-    std::vector<std::string> vector2;
+    std::vector<int> numbers;
 
-    std::cout << "Enter five strings for vector1: ";
-    for (int i = 0; i < 5; i++) {
-        std::string input;
+    std::cout << "Enter ten integers: ";
+    for (int i = 0; i < 10; i++) {
+        int input;
         std::cin >> input;
-        vector1.push_back(input);
+        numbers.push_back(input);
     }
 
-    std::cout << "Enter five strings for vector2: ";
-    for (int i = 0; i < 5; i++) {
-        std::string input;
-        std::cin >> input;
-        vector2.push_back(input);
-    }
+    removeDuplicates(numbers);
 
-    std::vector<std::string> intersection = intersect(vector1, vector2);
-
-    std::cout << "The common strings are: ";
-    for (const auto& element : intersection) {
-        std::cout << element << " ";
+    std::cout << "The distinct integers are: ";
+    for (const auto& number : numbers) {
+        std::cout << number << " ";
     }
     std::cout << std::endl;
 
