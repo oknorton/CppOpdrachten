e75b6dba096f47d27c8e2a730e60de1c025f6043
Oliver Norton <o.k.norton21@gmail.com>
2023-06-11 14:25:27 +0200

    Les 2 Opdracht 2

--- CppOpdrahctenV2/CppOpdrahctenV2/CppOpdrahctenV2.cpp
+++ CppOpdrahctenV2/CppOpdrahctenV2/CppOpdrahctenV2.cpp
@@ -1,26 +1,73 @@
 #include <iostream>
-#include <algorithm>
+#include <string>
 #include <vector>
 
-#include "StopWatch.h"
+//Met assistentie van ChatGPT
+bool isGene(const std::string& gene) {
+    std::string forbidden[] = { "ATG", "TAG", "TTA", "TGA" };
 
-int main() {
-    std::vector<int> numbers(100000);
-    for (int i = 0; i < 100000; ++i) {
-        numbers[i] = rand();
+    for (const std::string& triplet : forbidden) {
+        if (gene.find(triplet) != std::string::npos) {
+            return false;
+        }
     }
 
-    StopWatch stopwatch;
+    return gene.length() % 3 == 0;
+}
 
-    stopwatch.start();
+std::vector<std::string> findGenes(const std::string& genome) {
+    std::vector<std::string> genes;
 
-    std::sort(numbers.begin(), numbers.end());
+    int i = 0;
+    while (i < genome.length()) {
+        int startIndex = genome.find("ATG", i);
+        if (startIndex == std::string::npos) {
+            break;
+        }
+        startIndex += 3;
 
-    stopwatch.stop();
+        std::string list[] = { "TAG", "TTA", "TGA" };
+        int endIndex = genome.find(list[0], startIndex);
+        for (const std::string& end : list) {
+            int en = genome.find(end, startIndex);
+            if (en < endIndex && en != std::string::npos) {
+                endIndex = en;
+            }
+        }
+        endIndex += 1;
+        if (endIndex == std::string::npos) {
+            break;
+        }
 
-    long long elapsedTime = stopwatch.getElapsedTime();
+        int length = endIndex - startIndex;
+        std::string gene = genome.substr(startIndex, length);
 
-    std::cout << "Sorting time: " << elapsedTime << " milliseconds." << std::endl;
+        if (isGene(gene)) {
+            genes.push_back(gene);
+        }
+
+        i = endIndex + 3;
+    }
+
+    return genes;
+}
+
+int main() {
+    std::string genome;
+    std::cout << "Enter a genome string: ";
+    std::cin >> genome;
+
+    std::vector<std::string> genes = findGenes(genome);
+
+    if (genes.empty()) {
+        std::cout << "No gene found." << std::endl;
+    }
+    else {
+        std::cout << "Genes found:" << std::endl;
+        for (const std::string& gene : genes) {
+            std::cout << gene << std::endl;
+        }
+    }
 
     return 0;
 }
