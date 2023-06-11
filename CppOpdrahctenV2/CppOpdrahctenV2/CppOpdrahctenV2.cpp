6094dc47ff9a80e28f776def0eedaddc24bdf254
Oliver Norton <o.k.norton21@gmail.com>
2023-06-11 15:53:04 +0200

    Les 5 opdracht 4

--- CppOpdrahctenV2/CppOpdrahctenV2/CppOpdrahctenV2.cpp
+++ CppOpdrahctenV2/CppOpdrahctenV2/CppOpdrahctenV2.cpp
@@ -1,47 +1,36 @@
+#include <iostream>
+#include <thread>
+#include "semaphore.h"
 
-//De rule of 350 is een vuistregel die wordt gebruikt in softwareontwikkeling om te bepalen of een bepaalde klasse te complex is.
-//Volgens deze regel mag een klasse niet meer dan 350 regels code bevatten. Het doel van deze regel is om de
-//complexiteit van een klasse te beperken en de leesbaarheid,
-//onderhoudbaarheid en herbruikbaarheid van de code te verbeteren/
+// Semaforen voor synchronisatie
+semaphore resourceSemaphore(1);  // Er is één resource beschikbaar
+semaphore printSemaphore(1);     // Er is één printresource beschikbaar
 
-//In dit voorbeeld wordt een klasse genaamd MyClass gedefinieerd. De klasse heeft een
-//dynamisch gealloceerd array data en een variabele size. Het volgt de Rule
-//of 3 door een destructor, kopieconstructor en toekenning-operator te implementeren.
-class MyClass {
+void ThreadFunction(int threadId) {
+    resourceSemaphore.Wait();
 
-private:
-    int* data;
-    int size;
+    // Kritieke sectie
+    std::cout << "Thread " << threadId << ": Start" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "Thread " << threadId << ": Einde" << std::endl;
 
-public:
-    // Constructor
-    MyClass(int s) : size(s) {
-        data = new int[size];
-    }
+    resourceSemaphore.Signal();
 
-    // Destructor
-    ~MyClass() {
-        delete[] data;
-    }
+    printSemaphore.Wait();
 
-    // Kopieconstructor
-    MyClass(const MyClass& other) : size(other.size) {
-        data = new int[size];
-        for (int i = 0; i < size; i++) {
-            data[i] = other.data[i];
-        }
-    }
+    std::cout << "Thread " << threadId << ": Print" << std::endl;
 
-    // Toekenning-operator
-    MyClass& operator=(const MyClass& other) {
-        if (this != &other) {
-            delete[] data;
-            size = other.size;
-            data = new int[size];
-            for (int i = 0; i < size; i++) {
-                data[i] = other.data[i];
-            }
-        }
-        return *this;
-    }
-};
+    printSemaphore.Signal();
+}
+
+int main() {
+    std::thread t1(ThreadFunction, 1);
+    std::thread t2(ThreadFunction, 2);
+    std::thread t3(ThreadFunction, 3);
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    return 0;
+}
